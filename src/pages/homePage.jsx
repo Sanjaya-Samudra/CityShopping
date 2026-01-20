41b911fc35c1d266067284b50a7be1388f4a93db
fc115740Dharmasri <maneeshadharmasri@gmail.com>
2026-01-20 22:36:11 +0530

    Create homePage.jsx

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "../components/header";
+import ProductPage from "./productPage";
+import ProductOverview from "./productOverview";
+import CartPage from "./cart";
+import CheckoutPage from "./checkOut";
+import OrdersPage from "./ordersPage";
+import Home from "./homeContent";
+import AboutPage from "./aboutPage";
+import ContactPage from "./contactPage";
+import NotFoundPage from "./notFoundPage";
+
+export default function HomePage(){
+    return(
+        <div className="w-full h-full overflow-y-scroll max-h-full">
+            <Header/>
+            <div className="w-full min-h-[calc(100%-100px)]">
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/products" element={<ProductPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path="/overview/:productID" element={<ProductOverview/>} />
+                    <Route path="/cart" element={<CartPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                    <Route path="/orders" element={<OrdersPage />} />
+                    <Route path="/*" element={<NotFoundPage />} />
+                </Routes>
+            </div>
+        </div>
+    )
+}
